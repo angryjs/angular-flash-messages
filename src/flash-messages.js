d769b657c8062348e6d173b7e2978392a4809a1a
JSifalda <sifalda.jiri@gmail.com>
2013-10-24 20:43:13 +0200

    Fix messages reset

--- src/flash-messages.js
+++ src/flash-messages.js
@@ -39,7 +39,7 @@ angular.module('angryjs.flashMessages', [])
 
     Flash.prototype.show = function() {
       this.$rootScope.$broadcast('messages:show', messages);
-      this.reset();
+      messages = [];
     };
 
     Flash.prototype.reset = function() {
